76e9686599990b0d087c03530a51d88c9364fd8a
Richard Obermayr <6305520+oberrich@users.noreply.github.com>
2022-09-17 14:17:33 +0200

    Added ProgramOptions implementation

--- main.cpp
+++ main.cpp
@@ -4,6 +4,10 @@
 #include <format>
 #include <iostream>
 #include <optional>
+#include <string_view>
+#include <ranges>
+#include <concepts>
+#include <optional>
 
 #include <cstdio>
 #include <cinttypes>
@@ -13,17 +17,80 @@
 
 #include <Zydis/Zydis.h>
 
-#define CALLBACK_ASSERT(cond)          \
-  if (!(cond))                         \
-    return DisassembleStatus::kFailed;
+#include <format>
 
-enum class DisassembleStatus
-{
-    kContinue,
-    kFailed,
-    kSuccess,
-    kFollow,
-    kZydisError
+struct Option {
+  std::wstring_view name;
+  std::wstring_view desc;
+  bool value;
+};
+
+template <std::size_t N>
+struct ProgramOptions {
+  ProgramOptions(auto &&...opts)
+    : argc{0}
+    , argv{const_cast<decltype(argv)>(CommandLineToArgvW(GetCommandLineW(), &argc))}
+    , command{argv[0]}
+    , options{std::array{Option{L"help", L"Shows all available options."}, std::forward<decltype(opts)>(opts)...}}
+  {
+    constexpr auto wrap_sv_trim = [](auto const *str) constexpr noexcept {
+      std::wstring_view sv{str};
+      sv.remove_prefix((std::min)(sv.find_first_not_of(L'-'), std::size(sv)));
+      return sv;
+    };
+
+    for (auto const arg : std::span{argv + 1, argv + argc} | std::views::transform(wrap_sv_trim)) {
+      for (auto &option : options)
+        if (option.name == arg)
+          option.value = true;
+    }
+
+    if (decltype(auto) self = std::as_const(*this); self[L"help"].value)
+      print_help();
+  }
+
+  template <typename ValT>
+  constexpr std::optional<std::reference_wrapper<ValT>> get(std::wstring_view name) const {
+    if (auto result = std::ranges::find_if(options, [name](auto const &option) constexpr {
+              return option.name == name; });
+        result != std::end(options))
+      return {*result};
+    return {};
+  }
+
+  constexpr Option       &operator[](std::wstring_view name)       { return get<Option      >(name).value().get(); }
+  constexpr Option const &operator[](std::wstring_view name) const { return get<Option const>(name).value().get(); }
+
+  void print_help() const noexcept {
+    constexpr std::wstring_view program_name_friendly{L"Win11 Toggle Rounded Corners"};
+    constexpr std::wstring_view program_name{L"win11-toggle-rounded-corners.exe"};
+    constexpr std::wstring_view author{L"oberrich"};
+    constexpr std::wstring_view version{L"v1.1"};
+    constexpr std::wstring_view copyright_year{L"2022"};
+
+    std::wcout << std::format(L"{} {}\nCopyright (C) {} {}\n\n{} [options]\nOptions:\n",
+                              program_name_friendly, version, copyright_year, author, program_name);
+
+    for (auto const &option : options)
+      std::wcout << std::format(L"  --{: <20}: {}\n", option.name, option.desc);
+  }
+
+  int argc;
+  wchar_t const **argv;
+  std::wstring_view const command;
+  std::array<Option, N> options;
+};
+
+template <typename... Options>
+ProgramOptions(Options...) -> ProgramOptions<sizeof...(Options) + 1>;
+
+
+enum class DisassembleStatus {
+  kContinue,
+  kFailed,
+  kSuccess,
+  kFollow,
+  kZydisError
 };
 
 using DisassembleCallbackT = DisassembleStatus(ZydisDecodedInstruction const &, ZydisDecodedOperand const *, uint64_t &);
@@ -48,7 +115,7 @@ void zydis_print_instrn(ZydisDecodedInstruction const &instrn, ZydisDecodedOpera
 
 DisassembleStatus zydis_disassemble(uint64_t address, std::function<DisassembleCallbackT> const &callback) noexcept
 {
-    CALLBACK_ASSERT(address != 0);
+    if (!address) return DisassembleStatus::kFailed;
 
     ZydisDecodedInstruction instrn{};
     ZydisDecodedOperand operands[ZYDIS_MAX_OPERAND_COUNT_VISIBLE];
@@ -56,7 +123,8 @@ DisassembleStatus zydis_disassemble(uint64_t address, std::function<DisassembleC
     while (ZYAN_SUCCESS(ZydisDecoderDecodeFull(&decoder, reinterpret_cast<void const *>(address), 32, &instrn, operands, ZYDIS_MAX_OPERAND_COUNT_VISIBLE, ZYDIS_DFLAG_VISIBLE_OPERANDS_ONLY))) {
         if (auto status = callback(instrn, operands, address); status != DisassembleStatus::kContinue) {
             if (status == DisassembleStatus::kFollow) {
-                CALLBACK_ASSERT(ZydisCalcAbsoluteAddress(&instrn, operands, address, &address));
+                if (!ZydisCalcAbsoluteAddress(&instrn, operands, address, &address))
+                    return DisassembleStatus::kFailed;
                 continue;
             }
             return status;
@@ -96,7 +164,9 @@ std::optional<std::ptrdiff_t> locate_udwm_desktop_manager() noexcept
 
     Context ctx{};
     auto callback = [&ctx](auto const &instrn, auto const operands, auto &address) noexcept -> DisassembleStatus {
-        CALLBACK_ASSERT(instrn.mnemonic != ZYDIS_MNEMONIC_RET);
+        if (instrn.mnemonic == ZYDIS_MNEMONIC_RET)
+            return DisassembleStatus::kFailed;
+
         zydis_print_instrn(instrn, operands, address);
 
         if (!ctx.found_dm_create && instrn.mnemonic == ZYDIS_MNEMONIC_CALL) {
@@ -116,7 +186,7 @@ std::optional<std::ptrdiff_t> locate_udwm_desktop_manager() noexcept
     if (zydis_disassemble(dwm_client_startup, callback) != DisassembleStatus::kSuccess || !ctx.dm_instance)
         return {};
 
-    return { ctx.dm_instance - reinterpret_cast<uint64_t>(udwm_dll) };
+    return ctx.dm_instance - reinterpret_cast<uint64_t>(udwm_dll) ;
 }
 
 std::optional<uint64_t> find_module_base(DWORD pid, std::string_view module_name) noexcept
@@ -129,7 +199,7 @@ std::optional<uint64_t> find_module_base(DWORD pid, std::string_view module_name
             do {
                 if (module_name == entry.szModule) {
                     CloseHandle(snapshot);
-                    return { reinterpret_cast<uint64_t>(entry.modBaseAddr) };
+                    return reinterpret_cast<uint64_t>(entry.modBaseAddr) ;
                 }
             } while (Module32Next(snapshot, &entry));
         }
@@ -162,6 +232,8 @@ bool enable_privilege(LPCTSTR name) noexcept
 
 int main() try
 {
+  ProgramOptions const options{Option{L"no-autostart", L"Disables auto-start"}};
+
     if (!enable_privilege(SE_DEBUG_NAME))
         throw std::runtime_error(std::format("Failed enable {}!", SE_DEBUG_NAME));
 
